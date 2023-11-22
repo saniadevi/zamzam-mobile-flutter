ee5fbbcabd2a0e1e9fa11ecf0021776992e333ce
saniadevi <fzats44@gmail.com>
2023-11-22 10:04:36 +0700

    push project

---
+++ widgets/sample_container.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SampleContainer extends StatelessWidget {
+  const SampleContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
